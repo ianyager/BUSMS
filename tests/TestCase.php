391068c357ef678cc1169d72aaa042ddbd8f74b1
Alex Wright <alex@xeentech.com>
2020-08-17 18:21:59 +0200

    Some basic tests for credits and send

---
+++ tests/TestCase.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Blackthorne\VoodooSms\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+use Xeen\MockServerClient\Client as MockServerClient;
+
+class TestCase extends BaseTestCase
+{
+    protected MockServerClient $mockServerClient;
+
+    /**
+     * @before
+     */
+    public function setupMockServerClient()
+    {
+        $this->mockServerClient = new MockServerClient();
+    }
+
+    protected function setMockServerExpectation($expectaion)
+    {
+        $this->mockServerClient->addExpectation($expectaion);
+    }
+
+    protected function clearMockServerExpectaions()
+    {
+        $this->mockServerClient->reset();
+    }
+}
---
+++ tests/VoodooSmsApiClientTest.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace Blackthorne\VoodooSms\Tests;
+
+use Blackthorne\VoodooSms\Client;
+use Blackthorne\VoodooSms\VoodooSmsMessage;
+
+class VoodooSmsApiClientTest extends TestCase
+{
+    private Client $client;
+
+    public function setUp(): void
+    {
+        $this->client = new Client($this->clientConfig());
+        $this->clearMockServerExpectaions();
+    }
+
+    public function clientConfig()
+    {
+        return [
+            'base_url' => 'http://vm-xeendev2:1080',
+            'secret' => '1234',
+        ];
+    }
+
+    public function setSecret($secret) {
+        $this->client->setConfig([
+            'secret' => $secret,
+        ]);
+    }
+
+    /**
+     * Test get_credits
+     * @dataProvider getBalanceProvider
+     */
+    public function testGetBalance($message, $expected, $secret, $spec)
+    {
+        $this->setSecret($secret);
+        $this->setMockServerExpectation($spec);
+
+        $balance = $this->client->get_credits();
+        $this->assertEquals($expected, $balance, $message);
+    }
+
+    public function getBalanceProvider()
+    {
+        return [
+            [
+                "Should have 100 credits",
+                100,
+                "secret1",
+                [
+                    'id' => 'get_credits_100',
+                    'times' => [
+                        "remainingTimes" => 1,
+                        "unlimited" => false,
+                    ],
+                    'httpRequest' => [
+                        'path' => '/credits',
+                        'method' => 'GET',
+                        'headers' => [
+                            [
+                                'name' => 'Authorization',
+                                'values' => ['Bearer secret1'],
+                            ],
+                        ],
+                    ],
+                    'httpResponse' => [
+                        'body' => [
+                            'json' => [
+                                'amount' => 100,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            [
+                "Should have 10 credits",
+                10,
+                "secret2",
+                [
+                    'id' => 'get_credits_10',
+                    'times' => [
+                        "remainingTimes" => 1,
+                        "unlimited" => false,
+                    ],
+                    'httpRequest' => [
+                        'path' => '/credits',
+                        'method' => 'GET',
+                        'headers' => [
+                            [
+                                'name' => 'Authorization',
+                                'values' => ['Bearer secret2'],
+                            ],
+                        ],
+                    ],
+                    'httpResponse' => [
+                        'body' => [
+                            'json' => [
+                                'amount' => 10,
+                            ],
+                        ],
+                    ],
+                ],
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider sendProvider
+     */
+    public function testSend($count, $to, $from, $message, $secret, $spec)
+    {
+        $this->setSecret($secret);
+        $this->setMockServerExpectation($spec);
+        $message = new VoodooSmsMessage(compact('to', 'from', 'message'));
+        $result = $this->client->send($message);
+        $this->assertIsObject($result);
+        foreach (['count', 'body', 'credits', 'balance'] as $key) {
+            $this->assertObjectHasAttribute($key, $result);
+        }
+        $this->assertEquals($count, $result->count, 'Should have same count');
+    }
+
+    public function sendProvider()
+    {
+        return [
+            [
+                1,
+                '+447800000000',
+                'Test Sender',
+                'Hello World!',
+                'secret2',
+                [
+                    'id' => 'send_1',
+                    'httpRequest' => [
+                        'path' => '/sendsms',
+                        'method' => 'POST',
+                        'headers' => [
+                            [
+                                'name' => 'Authorization',
+                                'values' => ['Bearer secret2'],
+                            ],
+                        ],
+                        'body' => [
+                            'to' => '+447800000000',
+                        ],
+                    ],
+                    'httpResponse' => [
+                        'body' => [
+                            'json' => [
+                                "count" => 1,
+                                "originator" => "VoodooSMS",
+                                "body" => "Hello this is your SMS body",
+                                "scheduledDateTime" => 1537525949,
+                                "credits" => 1,
+                                "balance" => 2365,
+                                "messages" => [
+                                    [
+                                        "id" => "97709216074987x3NFD16GgkChK2E67441209181vapi",
+                                        "recipient" => 447800000000,
+                                        "reference" => null,
+                                        "status" => "PENDING_SENT",
+                                    ]
+                                ]
+                            ],
+                        ],
+                    ],
+                ]
+            ],
+        ];
+    }
+}
